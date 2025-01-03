15eb6534e3ce3faafdb3328b52232dd0f454d118
yakupaslantas <martyrerdixon@gmail.com>
2025-01-03 20:43:23 +0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_TECNO-LG6n-user
-add_lunch_combo omni_TECNO-LG6n-userdebug
-add_lunch_combo omni_TECNO-LG6n-eng
+add_lunch_combo twrp_TECNO-LG6n-user
+add_lunch_combo twrp_TECNO-LG6n-userdebug
+add_lunch_combo twrp_TECNO-LG6n-eng
